46071b778cbb8501fa9caa824e9d083f63e2bb30
yyzbf <1251234619@qq.com>
2021-11-14 21:55:15 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-11-14 19:17:23
+ * @LastEditTime: 2021-11-14 21:49:07
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \阶段五 Vue.js项目实战开发\前端vue移动端项目资料\vue移动端项目第一天\one\toutiao-m\src\App.vue
+-->
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <router-view>
+      <h1>黑马头条</h1>
+    </router-view>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-14 19:17:23
+ * @LastEditTime: 2021-11-14 21:52:57
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \阶段五 Vue.js项目实战开发\前端vue移动端项目资料\vue移动端项目第一天\one\toutiao-m\src\main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
